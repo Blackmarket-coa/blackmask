43ccdb997d29f16d78728ea4ec19de2010d481a7
rr-bw <102181210+rr-bw@users.noreply.github.com>
2026-05-20 13:49:01 -0700

    update PasswordRequest to require current hash

--- libs/angular/src/auth/password-management/change-password/default-change-password.service.ts
+++ libs/angular/src/auth/password-management/change-password/default-change-password.service.ts
@@ -76,12 +76,11 @@ export class DefaultChangePasswordService implements ChangePasswordService {
     );
 
     const request = new PasswordRequest(
+      currentAuthenticationData.masterPasswordAuthenticationHash,
       newAuthenticationData,
       newUnlockData,
       passwordInputResult.newPasswordHint,
     );
-
-    request.authenticateWith(currentAuthenticationData);
 
     await this.masterPasswordApiService.postPassword(request);
   }
